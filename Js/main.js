7d3ab006bb1e5c12f8dd614448098165d89ec149
Johana Usaquen <lizethwist694@gmail.com>
2025-07-29 23:05:31 -0500

    Avances del proyecto

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,29 @@
 fetch('./Components/navbar.html')
     .then(response => response.text())
     .then(data => {
-    document.getElementById('navbar').innerHTML = data;
-});
+        document.getElementById('navbar').innerHTML = data;
+    });
 
 fetch('footer.html')
     .then(response => response.text())
     .then(data => {
-    document.getElementById('footer').innerHTML = data;
+        document.getElementById('footer').innerHTML = data;
+    });
+
+// Para abrir la pantalla de agregar 
+
+document.querySelector('a[href="#addNewItem"]').addEventListener('click', function (e) {
+    e.preventDefault(); // evita el salto directo
+
+    const target = document.querySelector('#addNewItem');
+    const bsCollapse = new bootstrap.Collapse(target, {
+        toggle: true // muestra u oculta
+    });
+});
+
+
+document.querySelectorAll('.btn-delete').forEach(btn => {
+    btn.addEventListener('click', function () {
+        this.closest('.cardComplete').remove();
+    });
 });
